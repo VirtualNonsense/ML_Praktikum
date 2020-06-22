cb4b06655320b3e7fa2e9a932d4114fba7aea216
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-06-22 14:58:38 +0200

    adjusted file structure

--- E1-7-KMeans/KMeans.py
+++ E1-7-KMeans/KMeans.py
@@ -12,7 +12,7 @@ from matplotlib import colors as mpl_colors
 
 def generate_test_data(cluster_seeds, n, max_dif):
     dim = cluster_seeds.shape[1]
-    array = np.zeros((n*cluster_seeds.shape[0], dim))
+    array = np.zeros((n * cluster_seeds.shape[0], dim))
     index = 0
     for seed in cluster_seeds:
         for i in range(n):
@@ -23,10 +23,26 @@ def generate_test_data(cluster_seeds, n, max_dif):
 
 
 if __name__ == '__main__':
+    # set loglevel
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+    # generate color dict
+    labels = np.unique(list(range(cl_se.shape[0])))
+    colors = np.random.choice(list(mpl_colors.CSS4_COLORS.keys()), size=len(labels))
+    color_dict = dict(zip(labels, colors))
+
+    # init plot
+    plt.style.use("dark_background")
+    fig = plt.figure()
+    ax0 = fig.add_subplot(1, 1, 1)
+
+    # setup test data
     cl_se = np.array([[1, 1],
                       [1, 6]])
-
     tra_data = generate_test_data(cl_se, 6, 2)
 
+    # classify unknown data
 
+    # plot newly classified data
 
+    plt.show()
