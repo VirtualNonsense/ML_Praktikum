3e1de09f602f97bddd0b278ce5d95fa6d50f9a21
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-06-23 16:09:42 +0200

    slight refactoring, doc strings

--- E1-7-KMeans/KMeans.py
+++ E1-7-KMeans/KMeans.py
@@ -15,7 +15,7 @@ def generate_test_data(cluster_seeds: np.ndarray, n, max_dif):
     for seed in cluster_seeds:
         for i in range(n):
             while True:
-                v = seed + np.random.choice([-1, 1], 2) * max_dif * np.random.uniform(size=dim)
+                v = seed + np.random.choice([-1, 1], 2) * 2 * max_dif * np.random.uniform(size=dim)
                 if np.linalg.norm(seed - v, ord=2) <= max_dif:
                     break
             array[index] += v
@@ -63,12 +63,12 @@ class KMeansClassifier:
         self.method = method
 
         # get init code_book
-        self.init_code_book = cb0 if cb0 is not None else self.__generate_init_code_book(k, train_data.shape[1],
-                                                                                         train_data.min(),
-                                                                                         tra_data.max())
+        # self.init_code_book = cb0 if cb0 is not None else self.__generate_init_code_book(k, train_data.shape[1],
+        #                                                                                  train_data.min(),
+        #                                                                                  tra_data.max())
         self.init_code_book = cb0 if cb0 is not None \
             else train_data[np.random.choice(tra_data.shape[0], k), :] if train_data_init_cb \
-            else self.__generate_init_code_book(k, train_data.shape[1], train_data.min(), tra_data.max())
+            else self.__generate_init_code_book(k, train_data)
 
         # Optimize code book
         self.optimized_code_book, self.assignment_matrix, self.code_book_history = self.expectation_maximization(
@@ -128,25 +128,33 @@ class KMeansClassifier:
         return code_book, assignment_matrix, code_book_history
 
     @staticmethod
-    def __generate_init_code_book(k, dim, min_val, max_val):
+    def __generate_init_code_book(k, train_data):
         """
         sloppy method to generate the initial code book
         :param k: amount code book vectors
-        :param dim: amount of dimensions per vector
-        :param min_val: upper limit
-        :param max_val: lower limit
+        :param train_data:
         :return:
         """
+        # calc mean vector
+        td_mean = np.mean(train_data, axis=0)
+        # get max difference, that will be used as radius
+        max_dif = np.linalg.norm(tra_data - td_mean, axis=1).max()
+        dim = td_mean.shape[0]
         array = np.zeros((k, dim))
         for k_i in range(k):
-            array[k_i] += np.random.choice([-1, 1], 2) * np.random.choice([min_val, max_val], 2) * np.random.random(dim)
+            while True:
+                # get random v within a radius of max_dif
+                v = td_mean + np.random.choice([-1, 1], 2) * 2 * max_dif * np.random.uniform(size=dim)
+                if np.linalg.norm(v, ord=2) < max_dif:
+                    break
+            array[k_i] += v
         return array
 
     @staticmethod
     def __optimize_code_book(code_book: np.ndarray, train_data: np.ndarray, assignment_matrix: np.ndarray,
                              allow_code_book_pruning: bool = False):
         """
-
+        This function tries to to find the best code book for given assignment matrix and train data
         :param code_book:
         :param train_data:
         :param assignment_matrix:
@@ -164,14 +172,22 @@ class KMeansClassifier:
                 continue
             cb_ve += np.sum((as_m_i * train_data.transpose()).transpose(), axis=0) / np.sum(as_m_i)
 
+        # logging there is an unused cb vector
         if len(unused_cb_i) > 0:
             logging.warning(f"unused code_book entry {unused_cb_i}")
+            # prune cb if allowed (interesting idea but not recommended)
             if allow_code_book_pruning:
                 n_code_book = np.delete(n_code_book, unused_cb_i, axis=0)
                 logging.debug(f"removed {unused_cb_i}, new code book: {n_code_book}")
         return n_code_book
 
     def __generate_assignment_matrix(self, cluster_matrix: np.ndarray, data_matrix: np.ndarray):
+        """
+        this function tries to find the ideal assignments between training data and cluster vectors
+        :param cluster_matrix:
+        :param data_matrix:
+        :return:
+        """
         m = np.zeros((data_matrix.shape[0], cluster_matrix.shape[0]))
         for row_i, row in enumerate(m):
             ld_index = self.__get_lowest_distance_index(data_matrix[row_i], cluster_matrix)
@@ -179,6 +195,13 @@ class KMeansClassifier:
         return m
 
     def __get_lowest_distance_index(self, data_vector: np.ndarray, cluster_matrix: np.ndarray):
+        """
+        this function returns the index where the distance between a given data_vector and one entry of the cluster
+        matrix is minimal
+        :param data_vector:
+        :param cluster_matrix:
+        :return:
+        """
         lowest_distance = None
         lowest_distance_index = None
         for cluster_index, cluster_v in enumerate(cluster_matrix):
@@ -189,6 +212,14 @@ class KMeansClassifier:
         return lowest_distance_index
 
     def __J(self, code_book, assignment_matrix, train_data):
+        """
+        this function analyses the "quality" of the given combination, training data and assignment matrix
+        Lower is better
+        :param code_book:
+        :param assignment_matrix:
+        :param train_data:
+        :return:
+        """
         J = 0
         for tv_i, train_vector in enumerate(train_data):
             for cv_i, cluster_vector in enumerate(code_book):
@@ -206,10 +237,11 @@ if __name__ == '__main__':
     ax0 = fig.add_subplot(1, 1, 1)
 
     # setup test data
-    cl_se = np.array([[1, 1],
-                      [1, 6],
-                      [6, 6]])
-    tra_data = generate_test_data(cl_se, 100, 1)
+    # please notice, that in some cases the generated data is kinda hard to split.
+    # try again in this case :)
+    diff = 50
+    cl_se = generate_test_data(np.array([10, 10]).reshape((1, 2)), 3, diff)
+    tra_data = generate_test_data(cl_se, 100, diff/4)
 
     # generate color dict
     labels = np.unique(list(range(cl_se.shape[0])))
@@ -218,25 +250,36 @@ if __name__ == '__main__':
     else:
         colors = np.random.choice(list(mpl_colors.TABLEAU_COLORS.keys()), size=len(labels))
     color_dict = dict(zip(labels, colors))
-    legend_patches = []
 
     # init classifier
     classifier = KMeansClassifier(cl_se.shape[0], tra_data)
 
+    # get predicted assignment of testdata
     cl_se_predicted = classifier.predict_cluster(cl_se)
 
-    # generate test data
-    com = np.mean(classifier.optimized_code_book, axis=0)
-    # test_data = generate_test_data(com.reshape((1, 2)), 50, 3)
-    test_data = generate_test_data(classifier.optimized_code_book, 100, 4)
+    # calculate interesting points for drawing
+    cb_center_of_mass = np.mean(classifier.optimized_code_book, axis=0)
+    tra_data_center_of_mass = np.mean(tra_data, axis=0)
+    # generate data to get predictions for
+    test_data = generate_test_data(classifier.optimized_code_book, 100, diff/4)
 
+    # get prediction
     test_data_assigned = classifier.predict_cluster(test_data)
-    legend_patches.append(ax0.plot(com[0],
-                                   com[1],
-                                   linestyle=" ",
-                                   marker='+',
-                                   color="white",
-                                   label=f"center of mass")[0])
+
+    # plot data
+    legend_patches = [ax0.plot(cb_center_of_mass[0],
+                               cb_center_of_mass[1],
+                               linestyle=" ",
+                               marker='+',
+                               color="white",
+                               label=f"code_book mean")[0],
+                      ax0.plot(tra_data_center_of_mass[0],
+                               tra_data_center_of_mass[1],
+                               linestyle=" ",
+                               marker='x',
+                               color="white",
+                               label=f"trainings_data mean")[0]
+                      ]
 
     for i in range(classifier.assignment_matrix.shape[1]):
         assigned_cluster = cl_se_predicted[:, i] == 1
@@ -250,14 +293,14 @@ if __name__ == '__main__':
         legend_patches.append(ax0.plot(classifier.init_code_book[i, 0],
                                        classifier.init_code_book[i, 1],
                                        linestyle=" ",
-                                       marker='^',
+                                       marker='s',
                                        color=color_dict[labels[i]],
                                        label=f"initial code book vector {labels[i]}")[0])
 
         legend_patches.append(ax0.plot(classifier.optimized_code_book[i, 0],
                                        classifier.optimized_code_book[i, 1],
                                        linestyle=" ",
-                                       marker='x',
+                                       marker='X',
                                        color=color_dict[labels[i]],
                                        label=f"optimized code book vector {labels[i]}")[0])
 
